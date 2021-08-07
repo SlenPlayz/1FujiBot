26a41ecfbd9950d481bbc5f546c7b8ddf7a97f7f
SlenPlayz <67079195+SlenPlayz@users.noreply.github.com>
2021-08-07 19:21:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
       msg.reply('Schedule: \n https://drive.google.com/file/d/11ainApP0rLnYa-EHamhPvEC_15zOU97k/view \n Topics Tested \n Express: https://drive.google.com/file/d/1sBrsWBuTmtND7vJz_Ewiqdjf0EptR9NJ/view \n NA: https://drive.google.com/file/d/1phGObmzyzvKuvWNz8btK3BUE6kGAgWWS/view \n NT: https://drive.google.com/file/d/1phGObmzyzvKuvWNz8btK3BUE6kGAgWWS/view');
       break;
       case '!timetable':
-      msg.reply('Message that goes above image', {
+      msg.reply('Here is the timetable', {
         files: [
           "https://i.ibb.co/Zh8MY26/ED679-DCE-3-EEE-42-CE-8-D22-653-C5-D61-F268.jpg"
         ]})
