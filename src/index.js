2f4276416a1aa34ac436817b69a0847fb6e58471
SlenPlayz <67079195+SlenPlayz@users.noreply.github.com>
2021-08-08 13:58:17 +0000

    Added custom status

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity({name:'procrastination', type:'PLAYING'}); 
 })
 client.on('message', msg => {
   switch(msg.content){
